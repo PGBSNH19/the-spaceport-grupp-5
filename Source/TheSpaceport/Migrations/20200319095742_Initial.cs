bb7937f108f821dbbddb893796df2cca47b7352b
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-03-19 11:21:01 +0100

    @singelmamma

---
+++ Source/TheSpaceport/Migrations/20200319095742_Initial.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheSpaceport.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Houses",
+                columns: table => new
+                {
+                    HouseID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Room = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Houses", x => x.HouseID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    PersonID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    HouseID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.PersonID);
+                    table.ForeignKey(
+                        name: "FK_Persons_Houses_HouseID",
+                        column: x => x.HouseID,
+                        principalTable: "Houses",
+                        principalColumn: "HouseID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_HouseID",
+                table: "Persons",
+                column: "HouseID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "Houses");
+        }
+    }
+}
--- Source/TheSpaceport/Program.cs
+++ Source/TheSpaceport/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Data;
+
 
 
 namespace TheSpaceport
@@ -9,8 +14,33 @@ namespace TheSpaceport
         
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
             
+        }
+    }
+
+    public class Person
+    {
+        public int PersonID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    } 
+
+    public class House
+    {
+        public int HouseID { get; set; }
+        public int Room { get; set; }
+        public List<Person> Persons { get; set; }
+    }
+
+    public class MyContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<House> Houses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContext)
+        {
+            dbContext.UseSqlServer(@"Data Source=den1.mssql8.gear.host;Initial Catalog=thespaceportdb;User id=thespaceportdb;password=Ld0RW!-xKvLa;");
         }
     }
 }
