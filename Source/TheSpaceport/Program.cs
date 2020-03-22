3de07caf9b48d16d6dc8a3ece0060855b03c2ca9
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-03-22 10:57:36 +0100

    @andremorad

--- Source/TheSpaceport/Program.cs
+++ Source/TheSpaceport/Program.cs
@@ -13,7 +13,7 @@ using Newtonsoft.Json;
 
 namespace TheSpaceport
 {
-    internal class Program
+    public class Program
     {
         private static void Main(string[] args)
         {
@@ -27,15 +27,17 @@ namespace TheSpaceport
             //var personRequest = new RestRequest($"people/?search={test}", DataFormat.Json);
             //var personResponse = client.Execute(personRequest);
             //var person = JsonConvert.DeserializeObject<CharacterRoot>(personResponse.Content);
-            var test = new CreateCustomer().PersonControl().StarshipControl();
-
+            var test = new CreateCustomer().PersonControl().StarshipControl().Charge();
         }
     }
 
     public interface IAccessControl
     {
         IAccessControl PersonControl();
+
         IAccessControl StarshipControl();
+
+        IAccessControl Charge();
     }
 
     public class CreateCustomer : IAccessControl
@@ -43,51 +45,90 @@ namespace TheSpaceport
         public string name { get; set; }
         public string ship { get; set; }
 
-        RestClient client = new RestClient("https://swapi.co/api/");
+        private RestClient client = new RestClient("https://swapi.co/api/");
+
         public IAccessControl PersonControl()
         {
-            Console.Write("Please enter your name: ");
-            var personRequest = new RestRequest($"people/?search={Console.ReadLine()}", DataFormat.Json);
-            var personResponse = client.Execute(personRequest);
-            var person = JsonConvert.DeserializeObject<CharacterRoot>(personResponse.Content);
-            if(person.results.Count > 0)
+            bool a = true;
+            do
             {
-                Console.WriteLine($"Welcome {person.results[0].name}");
-                this.name = person.results[0].name;
-            }
-            else
-            {
-                Console.WriteLine("Access denied");
-            }
+                Console.Write("Please enter your name: ");
+                var personRequest = new RestRequest($"people/?search={Console.ReadLine()}", DataFormat.Json);
+                var personResponse = client.Execute(personRequest);
+                var person = JsonConvert.DeserializeObject<CharacterRoot>(personResponse.Content);
+                if (person.results.Count > 0)
+                {
+                    Console.WriteLine($"Welcome {person.results[0].name}");
+                    this.name = person.results[0].name;
+                    a = false;
+                }
+                else
+                {
+                    Console.WriteLine("Access denied");
+                }
+            } while (a);
             return this;
         }
+
         public IAccessControl StarshipControl()
         {
-            Console.Write("Please validate your starship: ");
-            var starshipRequest = new RestRequest($"starships/?search={Console.ReadLine()}", DataFormat.Json);
-            var starshipResponse = client.Execute(starshipRequest);
-            var starship = JsonConvert.DeserializeObject<StarshipRoot>(starshipResponse.Content);
-            if(starship.results.Count > 0)
+            bool a = true;
+            do
             {
-                Console.WriteLine($"{starship.results[0].name} ready for parking");
-                this.ship = starship.results[0].name;
+                Console.Write("Please validate your starship: ");
+                var starshipRequest = new RestRequest($"starships/?search={Console.ReadLine()}", DataFormat.Json);
+                var starshipResponse = client.Execute(starshipRequest);
+                var starship = JsonConvert.DeserializeObject<StarshipRoot>(starshipResponse.Content);
+
+                if (starship.results.Count > 0)
+                {
+                    Console.WriteLine($"{starship.results[0].name} ready for parking");
+                    this.ship = starship.results[0].name;
+                    a = false;
+                }
+                else
+                {
+                    Console.WriteLine("Unauthorised spaceship");
+                }
             }
-            else
+
+            while (a);
+            return this;
+        }
+
+        public IAccessControl Charge()
+        {
+            bool a = true;
+            int antalDagar = 0;
+            int b = 0;
+            string s;
+            do
             {
-                Console.WriteLine("Unauthorised spaceship");
-            }
+                Console.WriteLine("To park it cost 500 kr day.\nEnter how many days you want to park? ");
+                s = Console.ReadLine();
+                if (int.TryParse(s, out b))
+                {
+                    antalDagar = int.Parse(s);
+                    if (antalDagar > 0)
+                    {
+                        Console.WriteLine($"You will park {antalDagar} days");
+                        a = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You input was {antalDagar} and it´s not invalid input");
+                    }
+                }
+            } while (a);
             return this;
         }
     }
-
-    
 
     public class Travler
     {
         public string Name { get; set; }
-
     }
-   
+
     public class Person
     {
         public int PersonID { get; set; }
@@ -95,17 +136,19 @@ namespace TheSpaceport
         public int Age { get; set; }
     }
 
-    public class House
+    public class Ship
     {
-        public int HouseID { get; set; }
-        public int Room { get; set; }
+        public int ShipID { get; set; }
+        public string ShipName { get; set; }
+        public int ShipLength { get; set; }
+
         public List<Person> Persons { get; set; }
     }
 
     public class MyContext : DbContext
     {
         public DbSet<Person> Persons { get; set; }
-        public DbSet<House> Houses { get; set; }
+        public DbSet<Ship> Ships { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder dbContext)
         {
