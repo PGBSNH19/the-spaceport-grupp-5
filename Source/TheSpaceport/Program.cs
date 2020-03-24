c6e0e080f065cf6786543f0cb254275c6935853e
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-03-24 10:01:51 +0100

    @andremorad

--- Source/TheSpaceport/Program.cs
+++ Source/TheSpaceport/Program.cs
@@ -17,15 +17,15 @@ using System.IO;
 
 namespace TheSpaceport
 {
-    public class Program
+    public class Program : MyContext
     {
-        public static void Main(string[] args)
+        public void Main(string[] args)
         {
             //var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
             var environmentName = Environment.GetEnvironmentVariable("SettingsEnviroment");
             var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").AddJsonFile($"appsettings.{environmentName}.json", optional: true);
             var config = builder.Build();
-            var defaultConnectionString = config.GetConnectionString("DefaultConnection");
+            defaultConnectionString = config.GetConnectionString("DefaultConnection");
 
             var test = new CreateCustomer().PersonControl().AddFunds().StarshipControl().Charge().AddToDataBase();
         }
@@ -178,15 +178,15 @@ namespace TheSpaceport
         public int NumberOfDays { get; set; }
     }
 
-    public class MyContext : DbContext
-    {
-        public DbSet<DatabasePerson> Persons { get; set; }
-        public DbSet<DatabaseStarship> Spaceships { get; set; }
+    //public class MyContext : DbContext
+    //{
+    //    public DbSet<DatabasePerson> Persons { get; set; }
+    //    public DbSet<DatabaseStarship> Spaceships { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder dbContext)
-        {
-            //dbContext.UseSqlServer(@"Data Source=den1.mssql8.gear.host;Initial Catalog=thespaceportdb;User id=thespaceportdb;password=Ld0RW!-xKvLa;");
-            dbContext.UseSqlServer(defaultConnectionString);
-        }
-    }
+    //    protected override void OnConfiguring(DbContextOptionsBuilder dbContext)
+    //    {
+    //        //dbContext.UseSqlServer(@"Data Source=den1.mssql8.gear.host;Initial Catalog=thespaceportdb;User id=thespaceportdb;password=Ld0RW!-xKvLa;");
+    //        dbContext.UseSqlServer(defaultConnectionString);
+    //    }
+    //}
 }
