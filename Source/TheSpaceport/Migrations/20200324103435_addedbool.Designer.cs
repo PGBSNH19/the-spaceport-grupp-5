7953f544c31c65f33257c29cf76257c52655a25e
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-03-24 11:40:00 +0100

    @singelmamma

--- Source/TheSpaceport/Migrations/20200324103435_addedbool.Designer.cs
+++ Source/TheSpaceport/Migrations/20200324103435_addedbool.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TheSpaceport;
 
 namespace TheSpaceport.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20200324103435_addedbool")]
+    partial class addedbool
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Source/TheSpaceport/Program.cs
+++ Source/TheSpaceport/Program.cs
@@ -176,17 +176,18 @@ namespace TheSpaceport
         public string ShipName { get; set; }
         public int PricePerDay { get; set; }
         public int NumberOfDays { get; set; }
+        public bool Payed { get; set; }
     }
 
-    //public class MyContext : DbContext
-    //{
-    //    public DbSet<DatabasePerson> Persons { get; set; }
-    //    public DbSet<DatabaseStarship> Spaceships { get; set; }
+    public class MyContext : DbContext
+    {
+        public DbSet<DatabasePerson> Persons { get; set; }
+        public DbSet<DatabaseStarship> Spaceships { get; set; }
 
-    //    protected override void OnConfiguring(DbContextOptionsBuilder dbContext)
-    //    {
-    //        //dbContext.UseSqlServer(@"Data Source=den1.mssql8.gear.host;Initial Catalog=thespaceportdb;User id=thespaceportdb;password=Ld0RW!-xKvLa;");
-    //        dbContext.UseSqlServer(defaultConnectionString);
-    //    }
-    //}
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContext)
+        {
+            dbContext.UseSqlServer(@"Data Source=den1.mssql8.gear.host;Initial Catalog=thespaceportdb;User id=thespaceportdb;password=Ld0RW!-xKvLa;");
+
+        }
+    }
 }
